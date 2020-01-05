64b3e2fc09dbfe2181655776e52ef121971a48bd
Ofaloaf <Ofaloaf@users.noreply.github.com>
2020-01-05 18:43:49 -0500

    mp commit maybe?
    - Why does github say there's 2406 changed files
    - But I got the right checksum
    - Send help

--- ConcertOfCreatives/common/defines/00_converter.lua
+++ ConcertOfCreatives/common/defines/00_converter.lua
@@ -2,10 +2,10 @@
 --
 -- NDefines.NDiplomacy.MAX_CLIENT_STATES = 20
 
-NDefines.NCountry.CORE_COLONY = 2.5
-NDefines.NCountry.CORE_OVERSEAS = 2.5							
-NDefines.NCountry.CORE_SAME_REGION = 1
-NDefines.NCountry.CORE_SAME_CONTINENT = 1.5
+-- NDefines.NCountry.CORE_COLONY = 1.5
+-- NDefines.NCountry.CORE_OVERSEAS = 1.5							
+-- NDefines.NCountry.CORE_SAME_REGION = 1
+-- NDefines.NCountry.CORE_SAME_CONTINENT = 1.5
 NDefines.NCountry.REGULAR_COLONY_GROWTH = 5
 NDefines.NCountry.COLONIAL_NATION_GROWTH_IMPACT = 0.5
 NDefines.NCountry.OVERSEAS_CLIENT_STATES = 1
